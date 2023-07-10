46aab35be5f9614e9fc3874a7d9e70cea411aac7
Danush Gopinath <Danush>
2023-07-10 21:28:17 +0530

    Initial commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,56 @@
+NoOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<NoOfDrums; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var drumKey = this.innerHTML;
+        MakeSound(drumKey);
+        MakeAnimation(drumKey);
+    });
+}
+
+addEventListener("keydown", function(event){
+    var keyboardKey = event.key;
+    MakeSound(keyboardKey);
+    MakeAnimation(keyboardKey);
+});
+
+function MakeSound(key){
+    switch(key){
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+    }
+}
+
+function MakeAnimation(key){
+    var activeKey = document.querySelector("."+key);
+    activeKey.classList.add("pressed");
+    setTimeout(function(){
+        activeKey.classList.remove("pressed");
+    },200);
+}
